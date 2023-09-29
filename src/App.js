ce69dd064bc91d51a968f42c8e79b52bc2431f6b
Kumar MM <cryptokumaru@gmail.com>
2023-09-29 10:42:04 -0700

    sample code using react class components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from "react";
 class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement() {
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+
+  handleIncrement() {
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
+  }
   render() {
-    return <div>Hello World!</div>;
+    const date = new Date("jun 21 2027");
+    date.setDate(date.getDate() + this.state.count);
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          {date.toDateString()} [{this.state.count}]
+        </span>
+        <button onClick={this.handleIncrement}>+</button>
+      </div>
+    );
   }
 }
 
